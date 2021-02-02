4d44bbe43b5b1291e51b24aaa8e08cb89a972dd2
unknown <dezhi.wang@yankon.dc>
2021-02-02 17:23:38 +0800

    update code

--- common/src/main/java/com/wdz/common/base/room/BaseRespository.java
+++ common/src/main/java/com/wdz/common/base/room/BaseRespository.java
@@ -1,6 +1,12 @@
 package com.wdz.common.base.room;
 
+import io.reactivex.Scheduler;
 import io.reactivex.Single;
+import io.reactivex.SingleObserver;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * @Author dezhi.wang
@@ -9,7 +15,26 @@ import io.reactivex.Single;
 public abstract class BaseRespository<T,R extends BaseDao> {
     private R mDao;
 
-    public void insertItems(T... items,DatabaseOperationListener<T> databaseOperationListener){
+    public void insertItems(T... items, final DatabaseOperationListener<T> databaseOperationListener){
         Single single = mDao.insertItems(items);
+        single.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new SingleObserver() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onSuccess(@NonNull Object o) {
+                        databaseOperationListener.onSuccess();
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        databaseOperationListener.onFailure();
+                    }
+                })
+
     }
 }
